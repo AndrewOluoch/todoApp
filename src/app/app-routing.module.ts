ec20fc234f4344d06c0dd6d3cd25163a1154372c
Andrew Oluoch <oluoch3@gmail.com>
2020-05-08 12:41:54 -0400

    submit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TodosComponent } from './components/todos/todos.component';
+import { from } from 'rxjs';
+import { AboutComponent } from './components/pages/about/about.component';
+
+
+const routes: Routes = [
+{ path:'', component: TodosComponent },
+{ path:'about', component: AboutComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
